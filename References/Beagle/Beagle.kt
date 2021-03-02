d6d6e4ac2c731b677905b8fcb5fa970549693619
decorodrigues <falecomandresantos@gmail.com>
2021-03-02 10:11:44 -0300

    update beagle reference

--- References/Beagle/Beagle.kt
+++ References/Beagle/Beagle.kt
@@ -1,7 +1,13 @@
-Beagle
+Beagle [referencias atualizadas em https://github.com/dreewr/ServerDrivenUI]
 
 Referências
 [1] https://www.youtube.com/watch?v=kzkgNdoqPdg&ab_channel=VitorRamos
+[2] Server Driven UI - https://www.youtube.com/watch?v=nwwapsDq2uM&ab_channel=EscolaparaDesenvolvedores&t=0s
+
+[3] KotlinConf2019 - Lona Scaling Server Driven UI: https://www.youtube.com/watch?v=Ir8lq4rSyyc&ab_channel=JetBrainsTV
+
+[TODOS]
+	- Fazer um exemplo usando o Design System do iti
 
 [1]	//todo: fazer uma poc de backend 
 	- SpringInitializer
@@ -63,13 +69,95 @@ Referências
 						- id: server_driven_container
 					//progress pra indicar o carregamento dos dados
 					Progress
+		- No .kt da Activity: 
+
+			@BeagleComponent
+			class AppBeagleActivity: BeagleActivity(){
+				private val toolbar = ...
+				private val progressBar = ....
+
+				...
+
+				override fun getServerDrivenContainerId() = R.id.server_driven_container
+
+				override fun getToolbar() = toolbar
+
+				override fun onServerDrivenContainerStateChanged(state: ServerDriveState){
+					if(state is ServerDriveState.Loading){
+						progressBar.visibility = visivel ou nao	
+					} else if ( state is ServerDriveState.Error){
+						Toast.makeText(this, "Erro", ...)
+
+					}
+				}
+			}
 
 
+		- No styles.xml 
+			Mudar o parent do tema do app de DarkActionBar para NoActionBar para orientar o sistema a somente carregar as views do XML 
+
+		---> Depois de compilar/buildar o projeto
+
+		- Criar uma classe de Application
+
+			class MyApplication: Application(){
+				override fun onCreate(){
+					...
+					BeagleSetup().init(this)
+				}
+
+			}
+		- Adicionar no manifest em <application> com android:name=".MyApplication"
 
 
+		- Criando o fluxo 
+		MainActivity chama a minha BeagleActivity
+			- Na MainActivity coloco um botão que chama um intent com a request configurada...
 
+				button.setOnClickListener(){
+					startActivity(
+						BeagleActivity.newIntent(
+							this, 
+							ScreenRequest("/screen"))
+					)
+				}
 
+		De forma meio automágica o app vai plotar na tela o elemento configurado no Beagle
 
+[2] Server Driven UI
+	- Modelo tradicional, a configuração do layout é de responsabilidade da plataforma, e os dados são recebidos através do backend e são apresentados nessa tela. E.g.: Tela 1 vai ter um texto e uma imagem, eu chamo um endpoint que me retorna um JSON e eu ploto esses elementos na minha tela 
+	- No Server-Driven-UI, o JSON não retorna somente os dados mas sim como eles devem ser exibidos 
+		- Spotify, ITI, Airb'n'b, iFood, SoundCloud, Uber 
+		- Vantagens: 
+			- Se o sistema possui multiplas plataformas, você não precisa construir uma nova tela em multiplas plaformas. Com o Design System acordado entre as tres o teu BFF consegue entregar o que cada plataforma precisa rnde
+			- com uma única versão do app é possível ter várias experiências configuráveis para o usuário. 
+				- Exemplo do UBER para capitais ou interior
+				- Exemplo de uma feature exclusiva para uma cidade que mostra a feature usando a localização do usuário
+
+			- Configurar conteúdos exlusivos para demograficos diferentes ou datas especiais 
+			- Adicionar ou remover features sem necessariamente precisar atualizar a versão da loja 
+		- Como funciona
+			- Servidor monta um JSON de acordo com as especificações do usuário ou da logica
+			- Aplicativo recebe esse JSON e renderiza o que é descrito pelo JSON, o processamento dos dados não é mais feito pelo aplicativo
+				- Essa renderização preferencialmente é feita tendo em mente que o backend é um BFF [microsserviço que customiza as entregas do backend para cada interface do usuário] com um contrato bem definido de Desi
+				- e.g> 
+				 	{
+				 		"title": "Title"
+				 		"boxes": [
+				 			{
+				 				"type": "FieldComponent",
+				 				"title": "Component"
+				 			},
+							{	
+								"type": "TextComponent",
+				 				"title": "Component"
+				 			},
+				 		]
+
+					}
+				 - Essa renderização poderia ser diferente para um um usuário 2 por exemplo, baseado em alguma parametrização
+
+[3]
 
 
 
