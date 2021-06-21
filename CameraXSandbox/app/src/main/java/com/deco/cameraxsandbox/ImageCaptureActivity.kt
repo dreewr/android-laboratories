6fc15e4dd51e377bcf00b50ca94aa1b87cf23ff7
André Rodrigues <falecomandresantos@gmail.com>
2021-06-21 06:52:22 -0300

    adiciona funcionalidade de imagecapture

--- CameraXSandbox/app/src/main/java/com/deco/cameraxsandbox/ImageCaptureActivity.kt
+++ CameraXSandbox/app/src/main/java/com/deco/cameraxsandbox/ImageCaptureActivity.kt
@@ -1,11 +1,139 @@
 package com.deco.cameraxsandbox
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import com.deco.cameraxsandbox.databinding.ActivityImageCaptureBinding
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.RECORD_AUDIO
+import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.core.impl.VideoCaptureConfig
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.Executors
 
 class ImageCaptureActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityImageCaptureBinding
+    private var imagePreview: Preview? = null
+
+    private var imageCapture: ImageCapture? = null
+
+    //private var videoCapture: VideoCapture? = null
+
+    private val cameraExecutor = Executors.newSingleThreadExecutor()
+
+    private lateinit var outputDirectory: File
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_image_capture)
+        binding = ActivityImageCaptureBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+        outputDirectory = getOutputDirectory()
+
+        binding.cameraCaptureButton.setOnClickListener {
+            takePicture()
+        }
+    }
+
+    private fun takePicture() {
+        val file = createFile(
+                outputDirectory,
+                FILENAME,
+                PHOTO_EXTENSION
+        )
+        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+        imageCapture?.takePicture(outputFileOptions, cameraExecutor, object : ImageCapture.OnImageSavedCallback {
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                val msg = "Photo capture succeeded: ${file.absolutePath}"
+                binding.previewView.post {
+                    Toast.makeText(this@ImageCaptureActivity,msg, Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                val msg = "Photo capture failed: ${exception.message}"
+                binding.previewView.post {
+                    Toast.makeText(this@ImageCaptureActivity, msg, Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+        cameraProviderFuture.addListener({
+            imagePreview = Preview.Builder().apply {
+                setTargetAspectRatio(AspectRatio.RATIO_16_9)
+                //setTargetRotation(binding.previewView.display.rotation)
+            }.build()
+            imageCapture = ImageCapture.Builder().apply {
+                setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                setFlashMode(ImageCapture.FLASH_MODE_AUTO)
+            }.build()
+
+           /* videoCapture = VideoCapture.Builder().apply {
+                setTargetAspectRatio(AspectRatio.RATIO_16_9)
+            }.build()
+*/
+            val cameraProvider = cameraProviderFuture.get()
+            val camera = cameraProvider.bindToLifecycle(this, cameraSelector, imagePreview, imageCapture)
+            binding.previewView.implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+            imagePreview?.setSurfaceProvider(binding.previewView.surfaceProvider)
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_PERMISSIONS ->
+                if (allPermissionsGranted()) {
+                    startCamera()
+                } else {
+                    requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+                }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all { permissions ->
+        ContextCompat.checkSelfPermission(this, permissions) == PackageManager.PERMISSION_GRANTED
+    }
+    companion object {
+        private const val TAG = "ImageCaptureActivity"
+        private const val FILENAME = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val PHOTO_EXTENSION = ".jpg"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(CAMERA/*, RECORD_AUDIO*/)
+
+        fun createFile(baseFolder: File, format: String, extension: String) =
+                File(baseFolder, SimpleDateFormat(format, Locale.US)
+                        .format(System.currentTimeMillis()) + extension)
     }
 }
