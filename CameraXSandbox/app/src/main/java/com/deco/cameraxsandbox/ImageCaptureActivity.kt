f14dff478427ac87583066e72f4d12f3da733970
André Rodrigues <falecomandresantos@gmail.com>
2021-06-21 05:52:24 -0300

    cria activity para usecase de captura de imagem

---
+++ CameraXSandbox/app/src/main/java/com/deco/cameraxsandbox/ImageCaptureActivity.kt
@@ -0,0 +1,11 @@
+package com.deco.cameraxsandbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageCaptureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_capture)
+    }
+}
